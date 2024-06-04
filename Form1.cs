0c6c919db792787c0fb7a44fe7c50babba84ae00
rikki3 <103515296+rikki3@users.noreply.github.com>
2024-06-04 22:25:57 +0200

    Initial upload
    Project partially abandoned. Inferring context currently required. Tests with Large Language Models may result in revival for interesting features.

---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.IO;
+using AltoHttp;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using fileDownloader2;
+
+namespace fileDownloader
+{
+    public partial class Form1 : Form
+    {
+        public static bool animeTab = false;
+        public static bool episodeReady = false;
+        public static string scrapedURL = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        HttpDownloader httpDownloader;
+        bool downloading = false;
+        bool paused = false;
+        bool started = false;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (paused)
+            {
+                started = false;
+                lblStatus.Text = ("Stopping...");
+                httpDownloader.Pause();
+                //File.Delete($"{Application.StartupPath}\\Downloads\\{Path.GetFileName(txtURL.Text)}");
+                MessageBox.Show("No files has been deleted!");
+                MessageBox.Show("Your download has been stopped.");
+                txtURL.Text = "";
+                txtURL.ReadOnly = false;
+                progressBar.Value = 0;
+                lblSpeed.Text = ("0 MB/s");
+                lblDownloaded.Text = ("0 MB");
+                lblPercent.Text = ("0 %");
+                btnStart.Text = "&Start";
+                downloading = false;
+                paused = false;
+                lblStatus.Text = ("Inactive");
+            }
+            else if (downloading == false)
+            {
+                if (txtURL.TextLength < 10)
+                {
+                    MessageBox.Show("Enter a valid URL.");
+                }
+                else
+                {
+                    started = true;
+                    httpDownloader = new HttpDownloader(txtURL.Text, $"{Application.StartupPath}\\Downloads\\{Path.GetFileName(txtURL.Text)}");
+                    httpDownloader.DownloadCompleted += HttpDownloader_DownloadCompleted;
+                    httpDownloader.ProgressChanged += HttpDownloader_ProgressChanged;
+                    httpDownloader.Start();
+                    MessageBox.Show("Your download has been successfully initiated.");
+                }
+            }
+            else if (downloading)
+            {
+                started = false;
+                lblStatus.Text = ("Stopping...");
+                httpDownloader.Pause();
+                //File.Delete($"{Application.StartupPath}\\Downloads\\{Path.GetFileName(txtURL.Text)}");
+                MessageBox.Show("No files has been deleted!");
+                MessageBox.Show("Your download has been stopped.");
+                txtURL.Text = "";
+                txtURL.ReadOnly = false;
+                progressBar.Value = 0;
+                lblSpeed.Text = ("0 MB/s");
+                lblDownloaded.Text = ("0 MB");
+                lblPercent.Text = ("0 %");
+                btnStart.Text = "&Start";
+                downloading = false;
+                paused = false;
+                lblStatus.Text = ("Inactive");
+            }
+        }
+
+        private void HttpDownloader_ProgressChanged(object sender, AltoHttp.ProgressChangedEventArgs e)
+        {
+            progressBar.Value = (int)e.Progress;
+            lblPercent.Text = $"{e.Progress.ToString("0.00")} %";
+            lblSpeed.Text = string.Format("{0} MB/s", (e.SpeedInBytes / 1024d / 1024d).ToString("0.00"));
+            lblDownloaded.Text = string.Format("{0} MB", (httpDownloader.TotalBytesReceived / 1024d / 1024d).ToString("0.00"));
+            lblStatus.Text = "Downloading...";
+            downloading = true;
+            txtURL.ReadOnly = true;
+            btnStart.Text = ("&Stop");
+        }
+
+        private void HttpDownloader_DownloadCompleted(object sender, EventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                lblStatus.Text = "Completed";
+                lblPercent.Text = "100%";
+                MessageBox.Show("Your download has been completed.");
+                txtURL.Text = "";
+                txtURL.ReadOnly = false;
+                progressBar.Value = 0;
+                lblSpeed.Text = ("0 MB/s");
+                lblDownloaded.Text = ("0 MB");
+                lblPercent.Text = ("0 %");
+                btnStart.Text = "&Start";
+                downloading = false;
+                paused = false;
+                lblStatus.Text = ("Inactive");
+            });
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (started == true && paused == false)
+            {
+                if (downloading)
+                {
+                    if (httpDownloader != null)
+                        httpDownloader.Pause();
+                    lblStatus.Text = "Paused";
+                    paused = true;
+                    MessageBox.Show("Your download has been paused.");
+                }
+            }
+            else if (paused == false)
+            {
+                //MessageBox.Show("Your download is already paused.");
+            }
+            else
+            {
+                //MessageBox.Show("Start a download.");
+            }
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            if (started)
+            {
+                if (paused == true)
+                {
+
+                    if (httpDownloader != null)
+                        httpDownloader.Resume();
+                    lblStatus.Text = "Resuming...";
+                    System.Threading.Thread.Sleep(3000);
+                    paused = false;
+                    downloading = true;
+                    MessageBox.Show("Your download has been resumed.");
+                }
+                else
+                {
+                    //MessageBox.Show("Your file is already downloading.");
+                }
+            }
+            else
+            {
+                //MessageBox.Show("Start a download.");
+            }
+        }
+
+        private void btnDirectory_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", ($"{Application.StartupPath}\\Downloads"));
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAnime_Click(object sender, EventArgs e)
+        {
+            if (downloading)
+            {
+                MessageBox.Show("Wait for current download to complete.");
+            }
+            else
+            {
+                if (animeTab == false)
+                {
+                    var animeForm = new Form2();
+                    animeForm.Show();
+                    animeTab = true;
+                }
+                else if (animeTab == true)
+                {
+                }
+            }
+        }
+
+        private void btnAttach_Click(object sender, EventArgs e)
+        {
+            if (downloading)
+            {
+                MessageBox.Show("Wait for current download to complete.");
+            }
+            else
+            {
+                if (episodeReady == true)
+                {
+                    txtURL.Text = scrapedURL;
+                    episodeReady = false;
+                }
+                else if (episodeReady == false)
+                {
+                    MessageBox.Show("First scrape a link.");
+                }
+            }
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using fileDownloader;
+using System.Xml;
+using System.Net;
+
+namespace fileDownloader2
+
+{
+    public partial class Form2 : Form
+    {
+
+        public string linkURL = "";
+        public string searchedAnime = "";
+        public string selectedAnime = "";
+
+        public Form2()
+        {
+            InitializeComponent();
+            lstAnime.DoubleClick += new EventHandler(lstAnime_DoubleClick);
+            lblEpisode.Visible = false;
+            txtEpisode.Visible = false;
+            btnEpisode.Visible = false;
+            lblAnimeList.Visible = false;
+            lstAnime.Visible = false;
+            //text input: link to episode page (https://www1.gogoanime.ai/dragon-ball-z-episode-21)
+            //download the content from https://www1.gogoanime.ai/dragon-ball-z-episode-21
+            //$('li.dowloads > a').href
+            //once you've scraped this url using xpath(or something similiar): "https://streamani.net/download?id=MTQ5NTk=&typesub=Gogoanime-SUB&title=Dragon+Ball+Z+Episode+21"
+            //$('div.dowload > a').href
+            //same thing: scrape for "https://storage.googleapis.com/pure-meridian-315002/T8BKBPU8OIPC/23a_1623237790_14959.mp4" and voila!
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(Form2_Closed);
+        }
+
+        public void Form2_Load(object sender, EventArgs e)
+        {
+            this.Width = 281;
+        }
+
+        public void Form2_Closed(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.Show();
+            Form1.ActiveForm.WindowState = FormWindowState.Normal;
+            Form1.episodeReady = true;
+            Form1.animeTab = false;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            this.Width = 495;
+            lblEpisode.Visible = false;
+            txtEpisode.Visible = false;
+            btnEpisode.Visible = false;
+            lstAnime.Items.Clear();
+            lstLinks.Items.Clear();
+            string xpath = "//p[@class=\"name\"]/a";
+            searchedAnime = "https://www1.gogoanime.ai/search.html?keyword=" + txtAnime.Text;
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            WebClient webClient = new WebClient();
+            string htmlContent = webClient.DownloadString(searchedAnime);
+            doc.LoadHtml(htmlContent);
+            var nodes = doc.DocumentNode.SelectNodes(xpath);
+            foreach (var node in nodes)
+            {
+                string animeName = node.Attributes["title"].Value;
+                string animeLink = node.Attributes["href"].Value;
+                lstAnime.Items.Add(animeName);
+                lstLinks.Items.Add(animeLink.Remove(0,9));
+            }
+            lblAnimeList.Visible = true;
+            lstAnime.Visible = true;
+        }
+        private void lstAnime_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstAnime.SelectedItem != null)
+            {
+                this.Width = 705;
+                int selectionNumber = lstAnime.SelectedIndex;
+                lstLinks.SelectedIndex = selectionNumber;
+                selectedAnime = lstLinks.SelectedItem.ToString();
+                lblEpisode.Visible = true;
+                txtEpisode.Visible = true;
+                btnEpisode.Visible = true;
+            }
+        }
+
+        private void btnEpisode_Click(object sender, EventArgs e)
+        {  
+            string episodeLink = "https://www1.gogoanime.ai" + selectedAnime + "-episode-" + txtEpisode.Text;
+            string episodeXpath = "//li[@class=\"dowloads\"]/a";
+            string downloadLinkXpath = "//div[@class=\"mirror_link\"]/div[@class=\"dowload\"]/a";
+
+            HtmlAgilityPack.HtmlDocument episodeDoc = new HtmlAgilityPack.HtmlDocument();
+            WebClient episodeWebClient = new WebClient();
+            string episodeHtmlContent = episodeWebClient.DownloadString(episodeLink);
+            episodeDoc.LoadHtml(episodeHtmlContent);
+            var node = episodeDoc.DocumentNode.SelectSingleNode(episodeXpath);
+            var link = node.Attributes["href"].Value;
+            var downloadLinkDoc = new HtmlAgilityPack.HtmlDocument();
+            string downloadLinkHtmlContent = episodeWebClient.DownloadString(link);
+            downloadLinkDoc.LoadHtml(downloadLinkHtmlContent);
+            var downloadLinkNode = downloadLinkDoc.DocumentNode.SelectSingleNode(downloadLinkXpath);
+            var downloadLink = downloadLinkNode.Attributes["href"].Value;
+
+            linkURL = downloadLink;
+            Form1.scrapedURL = linkURL;
+            MessageBox.Show(Form1.scrapedURL);
+            lblReady.Visible = true;
+            btnCloseAnime.Visible = true;
+        }
+
+        private void btnCloseAnime_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+
+//https://www1.gogoanime.ai/one-piece-episode-
+//https://www1.gogoanime.ai/dragon-ball-z-episode-
+//selectedEpisode = txtEpisode.Text;
+//HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+//string URL = "" + selectedEpisode;
+//WebClient webClient = new WebClient();
+//string htmlContent = webClient.DownloadString(URL);
+//doc.LoadHtml(htmlContent);
+//string xpath = "//li[@class=\"dowloads\"]/a";
+//var node = doc.DocumentNode.SelectSingleNode(xpath);
+//var link = node.Attributes["href"].Value;
+//var episodeDoc = new HtmlAgilityPack.HtmlDocument();
+//string episodeHtmlContent = webClient.DownloadString(link);
+//episodeDoc.LoadHtml(episodeHtmlContent);
+//string episodeXpath = "//div[@class=\"mirror_link\"]/div[@class=\"dowload\"]/a";
+//var episodeNode = episodeDoc.DocumentNode.SelectSingleNode(episodeXpath);
+//var episodeLink = episodeNode.Attributes["href"].Value;
+
+//linkURL = episodeLink;
+//Form1.scrapedURL = linkURL;
+//lblReady.Visible = true;
